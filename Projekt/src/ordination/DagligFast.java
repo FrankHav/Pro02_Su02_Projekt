a768b2b11d4703feb97a475c0294af5d0b59df30
marti <martin.b.soenderby@gmail.com>
2022-03-03 11:20:46 +0100

    test

--- Projekt/src/ordination/DagligFast.java
+++ Projekt/src/ordination/DagligFast.java
@@ -1,29 +1,44 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DagligFast extends Ordination {
     // TODO
-    private double morgenAntal;
+    private Dosis morgenAntal;
     private double middagAntal;
     private double aftenAntal;
     private double natAntal;
     private Dosis[] dosisArray = new Dosis[4];
 
-    public DagligFast(double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
-        this.morgenAntal = morgenAntal;
+    public DagligFast(LocalDate startDen, LocalDate slutDen, double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
+        super(startDen, slutDen);
+        if (morgenAntal>0){
+            createDosis(LocalTime.of(8, 0), morgenAntal,0);
+        }
+        this.dosisArray[0] = cmorgenAntal;
         this.middagAntal = middagAntal;
         this.aftenAntal = aftenAntal;
         this.natAntal = natAntal;
     }
 
-    public void createDosis(){
+    public Dosis createDosis(LocalTime tid, double antal, int index){
+        Dosis dosis = new Dosis(tid, antal);
+        dosisArray[dosisArray.length-1] = dosis;
+        return dosis;
+    }
 
+    public Dosis[] getDosisArray() {
+        return dosisArray;
     }
 
     @Override
     public double samletDosis() {
-        return 0;
+        int samletDos = 0;
+        for (int i = 0; i < dosisArray.length;i++){
+            samletDos += dosisArray[i];
+        }
+        return ;
     }
 
     @Override
