c4fac0918320d77953be867e52dcef2940cf7411
madsj <eaamjf@students.eaaa.dk>
2022-03-03 10:36:49 +0100

    commit DagligSkaev

--- Projekt/src/ordination/DagligSkaev.java
+++ Projekt/src/ordination/DagligSkaev.java
@@ -1,12 +1,42 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination{
     // TODO
 
+    private LocalDate startDen;
+    private LocalDate slutDen;
+    private Patient patient;
+    private Laegemiddel laegemiddel;
+    private LocalTime[] klokkeSlet;
+    private double[] antalEnheder;
 
-    public void opretDosis(LocalTime tid, double antal) {
+
+    public void opretDosis(LocalTime tid, double antal,LocalDate startDen,LocalDate slutDen, Patient patient,
+                           Laegemiddel laegemiddel) {
+    Dosis dosis = new Dosis(tid, antal);
+    dosiser.add(dosis);
         // TODO
+    }
+
+    private final ArrayList<Dosis> dosiser = new ArrayList<>();
+
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
